148ed95923e351d95d618e9103b7dda085bd44b2
Thorung Boonkaew <thorung.b@ku.th>
2024-09-08 14:55:32 +0700

    Add assets and edit some components in Footer

--- b2d-venture/constants/index.ts
+++ b2d-venture/constants/index.ts
@@ -12,3 +12,41 @@ export const headerLinks = [
         route: '/about',
     },
 ]
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+    {
+      title: 'Learn More',
+      links: [
+        'About Hilink',
+        'Press Releases',
+        'Environment',
+        'Jobs',
+        'Privacy Policy',
+        'Contact Us',
+      ],
+    },
+    {
+      title: 'Our Community',
+      links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+    },
+  ];
+  
+  export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+      { label: 'Admin Officer', value: '123-456-7890' },
+      { label: 'Email Officer', value: 'hilink@akinthil.com' },
+    ],
+  };
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+      '/wordpress.svg',
+    ],
+  };
--- b2d-venture/tailwind.config.js
+++ b2d-venture/tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-// import { withUt } from 'uploadthing/tw';
 
 module.exports = {
   darkMode: ['class'],
