7f54adc7d985d46e74d2e8f67935246da364c726
Thorung Boonkaew <thorung.b@ku.th>
2024-09-09 11:32:44 +0700

    Make Login page responsive for any device

--- b2d-venture/app/(auth)/Login/page.tsx
+++ b2d-venture/app/(auth)/Login/page.tsx
@@ -3,69 +3,74 @@ import Link from "next/link";
 
 const Login = () => {
   return (
-    <div className="flex h-screen bg-[#FFF5EE]">
+    <div className="flex flex-col md:flex-row h-screen bg-[#FFF5EE]">
       {/* Left side */}
-      <div className="w-1/2 flex flex-col justify-center items-center p-8">
-        {/* <h2 className="text-2xl font-bold text-[#FF6347] mb-4">B2D Venture</h2>
-        <div className="relative w-64 h-64">
-          <img src="/api/placeholder/256/256" alt="Laptop" className="w-full h-full object-contain" />
-          <Rocket className="absolute bottom-0 left-1/2 transform -translate-x-1/2 text-[#FF69B4] w-24 h-24" />
-        </div> */}
+      <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-8 relative">
+        {/* Position the B2D Venture text */}
+        <h2 className="absolute top-10 left-20 text-2xl font-bold text-[#FF6347]">B2D Venture</h2>
+
+        {/* Placeholder for the image */}
+        <div className="relative w-[200px] h-[200px] md:w-[350px] md:h-[350px]">
+          <img
+            src="/assets/images/rocket-laptop.png" // Replace with the actual image path
+            alt="Rocket with Laptop"
+            className="w-full h-full object-contain"
+          />
+        </div>
       </div>
 
-      {/* Right side (with White background) */}
-      <div className="w-1/2 flex flex-col justify-center p-8 bg-white">
-        <h1 className="h1-welcome-back">Welcome back</h1>
-        <p className="p-regular-18 text-center mt-5">Welcome back! Please enter your details to get login to your account</p>
+      {/* Right side (Login form) */}
+      <div className="w-full md:w-1/2 flex flex-col justify-center p-8 bg-white">
+        <h1 className="text-3xl font-bold text-center text-[#FF6347]">Welcome back</h1>
+        <p className="text-center mt-5 text-lg">Welcome back! Please enter your details to log into your account</p>
 
         <form className="space-y-5">
-          <div className="flex-center">
+          <div className="flex justify-center">
             <input 
               type="text" 
               placeholder="Your username or email"
-              className="mt-20 w-[550px] h-[50px] p-4 border-2 border-[#D9D9D9] rounded-full focus:outline-none focus:border-[#D9D9D9]" />
+              className="mt-10 w-[300px] md:w-[450px] h-[50px] p-4 border-2 border-[#D9D9D9] rounded-full focus:outline-none focus:border-[#FF7A00]" />
           </div>
-          <div className="flex-center">
+          <div className="flex justify-center">
             <input 
               type="password" 
               placeholder="Your Password" 
-              className="w-[550px] h-[50px] p-4 border-2 border-[#D9D9D9] rounded-full focus:outline-none focus:border-[#D9D9D9]" />
+              className="w-[300px] md:w-[450px] h-[50px] p-4 border-2 border-[#D9D9D9] rounded-full focus:outline-none focus:border-[#FF7A00]" />
           </div>
           
-          <div className="flex justify-between items-center mt-5 w-[550px] mx-auto">
-            <label className="flex items-center text-lg">
+          <div className="flex justify-between items-center mt-5 w-[300px] md:w-[450px] mx-auto">
+            <label className="flex items-center text-sm md:text-lg">
               <input
                 type="checkbox"
                 className="mr-2 form-checkbox text-yellow-500 focus:ring-yellow-500 rounded-full"
               />
               Remember me
             </label>
-            <a href="#" className="text-blue-500 text-lg">
+            <a href="#" className="text-blue-500 text-sm md:text-lg">
               Forgot password?
             </a>
           </div>
 
-          
-          <div className="flex-center">
-            <Button asChild className="w-[450px] h-[50px] rounded-full text-white p-regular-18 bg-[#FF993B] hover:bg-[#FF7A00] mt-16">
+          <div className="flex justify-center mt-8">
+            <Button asChild className="w-[300px] md:w-[450px] h-[50px] rounded-full text-white bg-[#FF993B] hover:bg-[#FF7A00]">
                 <Link href="/">
                   Login
                 </Link>
             </Button>
           </div>
-          <div className="flex-center">
-            <Button className="w-[450px] h-[50px] border-2 border-[#D9D9D9] rounded-full text-[#1C0E0D] p-regular-18 bg-white hover:bg-[#D9D9D9]">
+          <div className="flex justify-center mt-5">
+            <Button className="w-[300px] md:w-[450px] h-[50px] border-2 border-[#D9D9D9] rounded-full text-[#1C0E0D] bg-white hover:bg-[#D9D9D9]">
               <img src="assets/icons/google-logo.png" alt="Google logo" height={23} width={23} className="mr-2" />
-                <Link href="/">
-                  Login with Google
-                </Link>
+              <Link href="/">
+                Login with Google
+              </Link>
             </Button>
           </div>
         </form>
 
-        <p className="mt-10 flex-center space-x-2">
-          <span>Don't have an account?</span>
-          <a href="/SignUp" className="text-[#FF6347] p-medium-16">Sign up now</a>
+        <p className="mt-10 flex justify-center space-x-2">
+          <span className="text-sm md:text-base">Don't have an account?</span>
+          <a href="/SignUp" className="text-[#FF6347] font-medium">Sign up now</a>
         </p>
       </div>
     </div>
