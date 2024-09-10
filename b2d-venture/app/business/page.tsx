6ee9672661ba4767113915f2b52a6b4092de44cc
Napasorn <tonnam.napasorn@gmail.com>
2024-09-11 00:28:55 +0700

    layout the business page

--- b2d-venture/app/business/page.tsx
+++ b2d-venture/app/business/page.tsx
@@ -6,6 +6,24 @@ import BusinessCard from "@/components/shared/BusinessCard";
 import "../globals.css";
 
 const Page = () =>{
+    const listItems: JSX.Element[] = [];
+    for (let i = 0 ; i < 12;i++ ){
+        listItems.push(
+            <BusinessCard
+              key={i} // Adding a unique key prop
+              className="mt-10"
+              coverimg="/images/b1.png"
+              profile="/images/p1.png"
+              name="RAD AI"
+              description="The Future of AI Marketing"
+              raised="100,000"
+              investors="20"
+              min="500"
+              valuation="20,000,000"
+            />
+          );
+        }
+      
     return(
         <>
         <Header />
@@ -15,18 +33,10 @@ const Page = () =>{
                 Discover tomorrow's leaders with <b>B2D Venture</b>. Unlock opportunities across Southeast Asia 
                 Dive into the dynamic business landscape, connect with emerging startups, and explore investment prospects that are shaping the region's future growth."
             </p>
-            <SearchBar className="mt-10"/>
-            <BusinessCard 
-                className="mt-10" 
-                coverimg="/images/b1.png"
-                profile="/images/p1.png"
-                name="RAD AI"
-                description="The Future of AI Marketing"
-                raised="100,000"
-                investors="20"
-                min="500"
-                valuation="20,000,000"
-                />
+            <SearchBar className="mt-10" />
+            <div className="flex flex-wrap gap-4 ">
+                {listItems}
+            </div>
         </div>
         </>
     );
