9d4b7ff4a2a1c23a18b955ca00c9da2de4e05499
Napasorn <tonnam.napasorn@gmail.com>
2024-09-11 23:26:01 +0700

    Fix card content's translate

--- b2d-venture/components/shared/BusinessCard.tsx
+++ b2d-venture/components/shared/BusinessCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const BusinessCard = ({className, coverimg, profile, name, description, raised, investors, min, valuation}) => {
  return (
     <div className ={className}>
-        <Card className= "overflow-hidden relative  w-[300px] h-[350px] bg-[#F2EBEB] rounded-xl">
+        <Card className= "shadow-md overflow-hidden relative  w-[300px] h-[350px] bg-[#F2EBEB] rounded-xl">
             <CardHeader className="relative flex-grow h-1/2 p-0 m-0">
                 <div className="relative w-full h-full">
                     <Image 
@@ -15,37 +15,41 @@ const BusinessCard = ({className, coverimg, profile, name, description, raised, 
                     />
                 </div>
             </CardHeader>
-            <div className="absolute left-[10%] top-[40%]">
-                <div className="relative w-[60px] h-[60px] ">
-                    <Image 
-                    src={profile} 
-                    style={{objectFit:"cover"}}
-                    alt="Business Image" 
-                    fill={true}
-                    className="rounded-t-xl"
-                    />
-                </div>
-            </div>
-            <div className="relative group ">
-                <CardContent className="relative bg-[#F2EBEB] h-[400px] .text-[24px]  hover:relative transition-all duration-400 hover:bottom-[120px] hover:border-2">
-                    <h2 className="mt-7 ml-2 font-semibold">{name}</h2>
-                    <p className="ml-2 text-[15px] font-normal">{description}</p>
-                    <hr className="mb-2 mt-[100px] border-t border-gray-300" />
-                    <div className="flex">
-                        <p className="ml-2 text-[15px] font-semibold">{raised}</p>
-                        <p className="ml-2">Raised</p>
-                    </div>
-                    <div className="flex">
-                        <p className="ml-2 text-[15px] font-semibold">{investors}</p>
-                        <p className="ml-2">investors</p>
-                    </div>
-                    <div className="flex">
-                        <p className="ml-2 text-[15px] font-semibold">{min}</p>
-                        <p className="ml-2">min. investment</p>
-                    </div>
-                    <div className="flex">
-                        <p className="ml-2 text-[15px] font-semibold">{valuation}</p>
-                        <p className="ml-2">valuation cap</p>
+            
+           
+            <div className="relative group">
+                <CardContent className="relative z-0 bg-[#F2EBEB] h-[400px] .text-[24px]  transition-transform duration-400 transform group-hover:-translate-y-[120px]">
+                    <div className="relative z-10 -top-7  ">
+                        <div className="relative w-[60px] h-[60px] ">
+                            <Image 
+                            src={profile} 
+                            style={{objectFit:"cover"}}
+                            alt="Business Image" 
+                            fill={true}
+                            className="rounded-t-xl"
+                            />
+                        </div>
+                        </div>
+                    <div className="relative -top-7">
+                        <h2 className="ml-2 font-semibold">{name}</h2>
+                        <p className="ml-2 text-[15px] font-normal">{description}</p>
+                        <hr className="mb-2 mt-[100px] border-t border-gray-300" />
+                        <div className="flex">
+                            <p className="ml-2 text-[15px] font-semibold">{raised}</p>
+                            <p className="ml-2">Raised</p>
+                        </div>
+                        <div className="flex">
+                            <p className="ml-2 text-[15px] font-semibold">{investors}</p>
+                            <p className="ml-2">investors</p>
+                        </div>
+                        <div className="flex">
+                            <p className="ml-2 text-[15px] font-semibold">{min}</p>
+                            <p className="ml-2">min. investment</p>
+                        </div>
+                        <div className="flex">
+                            <p className="ml-2 text-[15px] font-semibold">{valuation}</p>
+                            <p className="ml-2">valuation cap</p>
+                        </div>
                     </div>
                  </CardContent>
               
