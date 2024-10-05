12bdf2241f4066f6632f23d5abb7b23ab43007c9
Thorung Boonkaew <thorung.b@ku.th>
2024-10-05 21:10:13 +0700

    Create page for setting account

--- b2d-venture/app/(root)/settings/account/page.tsx
+++ b2d-venture/app/(root)/settings/account/page.tsx
@@ -1,9 +1,17 @@
-function page() {
+import { Separator } from "@/components/ui/separator"
+import { AccountForm } from "@/components/shared/account-form"
+
+export default function SettingsAccountPage() {
   return (
-    <div>
-      Hello
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Account</h3>
+        <p className="text-sm text-muted-foreground">
+          Update your account settings.
+        </p>
+      </div>
+      <Separator />
+      <AccountForm />
     </div>
   )
 }
-
-export default page
