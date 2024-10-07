442c1a053c3943b2c3ff3cc2b1797bb89e08f18c
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-10-07 16:13:16 +0700

    Add google login

--- b2d-venture/models/user.js
+++ b2d-venture/models/user.js
@@ -4,11 +4,9 @@ const userSchema = mongoose.Schema(
     {
       username: {
         type: String,
-        required: true
       },
       password: {
         type: String,
-        required: true
       },
       email: {
         type: String,
