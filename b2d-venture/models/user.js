8593140918d9e182016a91f0c86cdc6743c89fa0
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-10-10 12:06:28 +0700

    Let default role of user be investor

--- b2d-venture/models/user.js
+++ b2d-venture/models/user.js
@@ -14,6 +14,8 @@ const userSchema = mongoose.Schema(
       },
       role: {
         type: String,
+        enum: ['admin','investor','business'],
+        default: 'investor'
       },
       contact_information: {
         type: Map,
