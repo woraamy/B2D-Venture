ee9aef768a8c36b6f85cf4b72a693ec5dc84d0ee
Napasorn TEVARUT <napasorn.te@ku.th>
2024-10-11 21:15:18 +0700

    add fetchingData api for all object

--- b2d-venture/app/api/fetchingData/RaiseCampaign/route.ts
+++ b2d-venture/app/api/fetchingData/RaiseCampaign/route.ts
@@ -2,15 +2,14 @@ import Business from "@/models/Business";
 import RaiseCampaign from "@/models/RaiseCampaign";
 import connect from "@/lib/connectDB";
 import { NextResponse, NextRequest } from "next/server";
-export async function GET(req: Request, { params }) {
-    const { id } = params;
+export async function GET() {
     try{
         await connect();
-        const data = await RaiseCampaign.findById(id).populate('business_id').lean();
+        const data = await RaiseCampaign.find().populate('business_id').lean();
         if (data) {
             return NextResponse.json({ data }); 
         }
-        return NextResponse.json({ message: `RaiseCampaign ${id} not found` });
+        return NextResponse.json({ message: `RaiseCampaign not found` });
     } catch (error){
         return NextResponse.json({ message: error });  
     }  
