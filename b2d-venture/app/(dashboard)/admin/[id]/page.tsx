160b7879b50478717fe2c7a764f31b20d94d42ff
Napasorn <tonnam.napasorn@gmail.com>
2024-10-12 18:25:55 +0700

    fetch data to report card

--- b2d-venture/app/(dashboard)/admin/[id]/page.tsx
+++ b2d-venture/app/(dashboard)/admin/[id]/page.tsx
@@ -3,9 +3,28 @@ import { promises as fs } from "fs";
 import { CgProfile } from "react-icons/cg";
 import Image from "next/image";
 import ReportCard from "@/components/shared/ReportCard";
+import connect from "@/lib/connectDB";
+import User from "@/models/user"
+import RaiseCampaign from "@/models/RaiseCampaign";
 
 export default async function Page({ params }) {
+    await connect();
     const {id} = params;
+
+    const activeInvestor = await User.countDocuments({role:'investor'})
+    const activeBusiness = await User.countDocuments({role:'business'})
+    const activeCampaign = await RaiseCampaign.countDocuments()
+    const [totalRaised] = await RaiseCampaign.aggregate(
+        [{
+            $group:{
+                _id: null,
+                total: {
+                    $sum: "$raised"
+            }
+            }
+        }]
+    )
+ 
 
     return(
         <>
@@ -13,8 +32,11 @@ export default async function Page({ params }) {
             {/* <div id="bar graph" className="flex w-[55vw] h-1/2 border-r-2 border-b-2"> */}
                 <div className="mt-5 ml-10">
                     <h1 className="text-[32px] font-bold">Dashboard</h1>                    
-                    <div className="ml-3">
-                    <ReportCard name='Active Investor' amout='100000'/>
+                    <div className="ml-3 flex">
+                    <ReportCard name='Active Investor' amout={activeInvestor}/>
+                    <ReportCard name='Active Business' amout={activeBusiness}/>
+                    <ReportCard name='Active Raise Campaign' amout={activeCampaign}/>
+                    <ReportCard name='Total Raised' amout={totalRaised.total}/>
                     </div>
                 </div>
             {/* </div> */}
