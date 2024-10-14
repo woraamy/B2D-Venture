963f86b6db6b7896d557b564c58f745c67129383
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-10-14 15:43:00 +0700

    Delete console.log lines

--- b2d-venture/components/charts/AdminChart.tsx
+++ b2d-venture/components/charts/AdminChart.tsx
@@ -51,14 +51,12 @@ export function AdminChart({className, data}) {
           data.splice(i, 0, { _id: { year: 2024, month: month.monthNum }, raised: 0, profit: 0 });
         }
       });
-    console.log(data)
     const chartData =  data.map((data,index)=>({
         month: months[index].month, 
         raised: data.raised,
 
         profit: data.profit,
   }));
-  console.log(chartData)
   return (
     <Card className={className} >
       <CardHeader>
