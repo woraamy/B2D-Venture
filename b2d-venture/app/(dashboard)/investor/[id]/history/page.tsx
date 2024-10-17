c35c380f577578a61eaa3b00bd76edce07dadb35
Napasorn TEVARUT <napasorn.te@ku.th>
2024-10-17 14:43:20 +0700

    add chart in history page

--- b2d-venture/app/(dashboard)/investor/[id]/history/page.tsx
+++ b2d-venture/app/(dashboard)/investor/[id]/history/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/shared/Header"
 import TableCard from "@/components/shared/InvestorDashboard/TableCard";
 import Investment from "@/models/Investment";
 import InvestHistoryCard from "@/components/shared/InvestorDashboard/InvestHistoryCard";
-
+import { OverviewChart } from "@/components/charts/overviewchart";
 function getOverviewData(data){
     const totalRaised = data.reduce((total, cur)=>{
         const business = cur.raise_campaign_id.business_id._id;
@@ -32,6 +32,32 @@ async function fetchInvestmentData(id) {
     const res = await response.json();
     return res.data || [];
 }
+
+function getPieChartData(data) {
+    const totalRaised = data.reduce((total, cur) => {
+        const business = cur.raise_campaign_id.business_id._id;
+        const name = cur.raise_campaign_id.business_id.BusinessName;
+        const amount = cur.amount;
+
+        if (!total[business]) {
+            total[business] = {
+                business: name,
+                raised: 0,
+            };
+        }
+        total[business].raised += amount;   
+        return total;
+
+    }, {});
+
+    const chartData = Object.values(totalRaised);
+    const pieData = chartData.map((item, index) => ({
+        ...item,  
+        fill: `var(--color-chart${index % 5 + 1})` 
+    }));
+    return pieData;
+}
+
 export default async function Page({params}) {
     const {id} = params;
     const investment = await fetchInvestmentData(id)
@@ -54,11 +80,14 @@ export default async function Page({params}) {
         {value:"Equity Stake", type:"text"},
         {value:"Shared recieve", type:"text"}
         ]
+    const pieChartdata = getPieChartData(investment)
     return(
         <div>
             <div className="ml-[6%] mt-10">
                 <h1 className="font-bold text-3xl">Overview Investment</h1>
                 <div className="mt-7 flex flex-wrap gap-3">
+                    <div><OverviewChart chartData={pieChartdata}/></div>
+                    <div className='ml-5'>
                     {overview.map((item, index)=>(   
                         <InvestHistoryCard 
                         key = {index}
@@ -71,10 +100,12 @@ export default async function Page({params}) {
                         shared={(item.totalRaised/item.shared).toFixed(2).toLocaleString()}
                         className="relative py-2"
                         />
-                    ))}</div>
+                    ))}
+                    </div>
+                    </div>
                 <h1 className="font-bold mt-7 text-3xl">Invest History</h1>
                 <TableCard data={headData} className='mt-7 mb-5' valueClassname='font-semibold'/>
-                <div>
+                <div className='mb-10'>
                     {
                         data.map((item,index)=>(
                             <TableCard key={index} data={item} className='mt-3' valueClassname='font-semibold'/>
