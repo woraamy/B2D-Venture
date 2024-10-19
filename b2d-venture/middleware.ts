9a5e0ae7e06d7b4b8ac32d65aa2b33561727072f
Napasorn <tonnam.napasorn@gmail.com>
2024-10-19 23:57:03 +0700

    fix business's middleware

--- b2d-venture/middleware.ts
+++ b2d-venture/middleware.ts
@@ -1,14 +1,16 @@
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
+import User from '@/models/user';
+import Business from '@/models/Business';
 
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   const { pathname } = req.nextUrl;
 
-  // if (!token) {
-  //   return NextResponse.redirect(new URL('/login', req.url));
-  // }
+  if (!token) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
 
   if (pathname.startsWith('/dashboard/admin')) {
     if (token.role !== 'admin') {
@@ -18,9 +20,8 @@ export async function middleware(req) {
 
   if (pathname.startsWith('/dashboard/business')) {
     const pathParts = pathname.split('/');
-    const businessId = pathParts[pathParts.length - 1];
-
-    if (token.role !== 'business' || token.businessId !== businessId) {
+    const businessId = pathParts[3];
+    if (token.role !== 'business') {
       return NextResponse.redirect(new URL('/403', req.url));
     }
   }
