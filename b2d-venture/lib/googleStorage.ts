09147824d74671045a1d7954d471671455658eec
Napasorn <tonnam.napasorn@gmail.com>
2024-10-20 21:06:48 +0700

    add google upload and get url action

--- b2d-venture/lib/googleStorage.ts
+++ b2d-venture/lib/googleStorage.ts
@@ -1,10 +1,13 @@
+'use server';
+
 import { Storage, Bucket} from '@google-cloud/storage';
 import fs from 'fs';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 const port = process.env.PORT;
 const keyFile = JSON.parse(process.env.KEY_FILE);
 const projectId = process.env.PROJECT_ID;
-const bucketName = process.env.BUCKET_NAME;
+const bucketName = process.env.DATAROOM_BUCKET_NAME;
 
 class GoogleStorage {
     storage: Storage;
@@ -19,8 +22,25 @@ class GoogleStorage {
         this.bucket = this.storage.bucket(bucketName);
      }
 
-  getPublicUrl(fileName) {
+  getPublicUrl(fileName:String) {
     return `https://storage.googleapis.com/${this.bucketName}/${fileName}`
+  }
+
+  async uploadFile(file:File){
+    if (!file){
+      throw new Error('No file provieded'); 
+    }
+    if (file.size < 1){
+      throw new Error('File is empty');
+    }
+    try {
+      const buffer = await file.arrayBuffer();
+      await this.bucket.file(file.name).save(Buffer.from(buffer))
+      return true
+    } catch (error) {
+      console.log(error)
+      return false 
+    }
   }
 
 }
