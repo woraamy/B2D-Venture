4a2bfd58ec50d329cf508572c9c5fc37be1db51d
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-10-21 10:45:35 +0700

    Change path for investor side nav

--- b2d-venture/components/shared/InvestorDashboard/InvestorSideNav.tsx
+++ b2d-venture/components/shared/InvestorDashboard/InvestorSideNav.tsx
@@ -8,10 +8,10 @@ import { IoIosSettings } from "react-icons/io";
 
 export default function InvestorSidenav({id}) {
     const pathname = usePathname();
-    const investpage = pathname === `dashboard/investor/${id}` ? "bg-[#FFF8F2] text-[#FF553E]  rounded-xl" : "" ;
-    const historypage = pathname.startsWith(`dashboard/investor/${id}/history`) ? "bg-[#FFF8F2] text-[#FF553E] p-3 rounded-xl" : "";
-    const informationpage = pathname.startsWith(`dashboard/investor/${id}/sharedInformation`) ? "bg-[#FFF8F2] text-[#FF553E] p-3 rounded-xl" : "";
-    const setting = pathname.startsWith(`dashboard/settings/account`) ? "bg-[#FFF8F2] text-[#FF553E] p-3 rounded-xl" : "";
+    const investpage = pathname === `/dashboard/investor/${id}` ? "bg-[#FFF8F2] text-[#FF553E]  rounded-xl" : "" ;
+    const historypage = pathname.startsWith(`/dashboard/investor/${id}/history`) ? "bg-[#FFF8F2] text-[#FF553E] p-3 rounded-xl" : "";
+    const informationpage = pathname.startsWith(`/dashboard/investor/${id}/sharedInformation`) ? "bg-[#FFF8F2] text-[#FF553E] p-3 rounded-xl" : "";
+    const setting = pathname.startsWith(`/dashboard/settings/account`) ? "bg-[#FFF8F2] text-[#FF553E] p-3 rounded-xl" : "";
 
     return(
         <div className="overflow-hidden flex bg-white sticky top-0 left-0 h-[100vh] w-[20%] xl:w-[15%] text-[#A6ABC8]">
@@ -19,15 +19,15 @@ export default function InvestorSidenav({id}) {
                 <span className="p-3">MENU</span>
                 <div className={`flex p-3 mt-2 ${investpage}`}>
                     < BsFileBarGraphFill />
-                    <Link href={`dashboard/investor/${id}`} className="ml-2">Dashboard</Link>
+                    <Link href={`/dashboard/investor/${id}`} className="ml-2">Dashboard</Link>
                 </div>
                 <div className={`flex mt-2 p-3 ${historypage}`}>
                     < MdWorkHistory />
-                    <Link href={`dashboard/investor/${id}/history`} className="ml-2">Invest History</Link>
+                    <Link href={`/dashboard/investor/${id}/history`} className="ml-2">Invest History</Link>
                 </div>
                 <div className={`flex mt-2 p-3 ${informationpage}`}>
                     < RiInformation2Fill />
-                    <Link href={`dashboard/investor/${id}/sharedInformation/status`} className="ml-2">Shared Information</Link>
+                    <Link href={`/dashboard/investor/${id}/sharedInformation/status`} className="ml-2">Shared Information</Link>
                 </div>
                 <div className={`flex mt-2 p-3 ${setting}`}>
                     < IoIosSettings />
