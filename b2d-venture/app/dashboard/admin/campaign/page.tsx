7ad8194e8d94c4a5bf068a772987f51486827d1e
Napasorn <tonnam.napasorn@gmail.com>
2024-10-25 23:20:09 +0700

    add raise campaign manage page

--- b2d-venture/app/dashboard/admin/campaign/page.tsx
+++ b2d-venture/app/dashboard/admin/campaign/page.tsx
@@ -8,23 +8,26 @@ import Filter from "@/components/ui/filter";
 import SearchBar from "@/components/ui/searchbar";
 import PaginationTable from "@/components/shared/PaginationTable";
 export default async function Page() {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/fetchingData/User`, { next: { tags: ['collection'] } });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/fetchingData/RaiseCampaign`, { next: { tags: ['collection'] } });
     const res = await response.json();
-    const user = res.data || []
+    const campaign = res.data || []
+    console.log(campaign)
   
-    const data = user.map((item,index)=>(
+    const data = campaign.map((item,index)=>(
         [
-            {value: item.username, type:"text"},
-            {value: item.email, type:"text"},
-            {value: item.role, type:"text"},
+            {value: {src:item.business_id.profile, text:item.business_id.BusinessName},type:"image"},
+            {value: item.start_date, type:"text"},
+            {value: item.end_date, type:"text"},
+            {value: {isHave: true,text: "Detail"}, type: "button"},
             {value: {isHave: true,text: "Edit"}, type: "button"},
             {value: {isHave: true,text: "Delete"}, type: "button"},
         ]
     ))
     const headData = [
-        {value:"User", type:"text"}, 
-        {value:"Email", type:"text"},
-        {value:"Role", type:"text"},
+        {value:"Business", type:"text"}, 
+        {value:"Start Date", type:"text"},
+        {value:"End Date", type:"text"},
+        {value:"Detail", type:"text"},
         {value:"Edit", type:"text"},
         {value:"Delete", type:"text"},
     ]
@@ -32,10 +35,10 @@ export default async function Page() {
     return(
         <div>
             <div className="ml-[6%] mt-10">
-                <h1 className="font-bold text-3xl">User Mangement</h1>
+                <h1 className="font-bold text-3xl">Business's raise campaign Mangement</h1>
                 <div className="flex mt-5 gap-5">
-                    <Button>+ Add New User</Button>
-                    <SearchBar text='Search user'/>
+                    <Button>+ Add Raised Campaign</Button>
+                    <SearchBar text='Search Raised Campaign'/>
                     <Filter className="" />
                 </div>
                 <TableCard data={headData} className='mt-7' valueClassname='font-semibold'/>
