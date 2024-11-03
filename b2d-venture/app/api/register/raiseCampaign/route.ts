e3cb7241d7257a13018324619d7ec460296774c7
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-11-03 15:56:36 +0700

    Add api route for creating raise campaign

---
+++ b2d-venture/app/api/register/raiseCampaign/route.ts
@@ -0,0 +1,72 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/lib/connectDB";
+import RaiseCampaign from "@/models/RaiseCampaign";
+
+export async function POST(req: NextRequest) {
+
+
+  try {
+    // Parse the request body
+    const {
+      business_id,
+      min_investment,
+      max_investment,
+      goal,
+      description,
+      files,
+      start_date,
+      end_date,
+      status,
+    } = await req.json();
+
+    // Log the incoming data to check for missing fields
+    console.log("Request body:", {
+        business_id,
+        min_investment,
+        max_investment,
+        goal,
+        description,
+        files,
+        start_date,
+        end_date,
+        status,
+    });
+
+    // Connect to the database
+    await connectDB();
+
+    // If not approved, create a pending business registration request
+    const newRequest = new RaiseCampaign({
+        business_id,
+        min_investment,
+        max_investment,
+        goal,
+        description,
+        files,
+        start_date,
+        end_date,
+        status,
+    });
+
+    // Save the new request to the database
+    await newRequest.save();
+    console.log("Raise campaign created successfully");
+
+    // Return success response
+    return NextResponse.json(
+      { message: "Business registration request submitted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error submitting business registration request:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error", error: error.message },
+      { status: 500 }
+    );
+  }
+}
+
+// Handle unsupported methods
+export async function OPTIONS() {
+  return NextResponse.json({ message: "Method Not Allowed" }, { status: 405 });
+}
