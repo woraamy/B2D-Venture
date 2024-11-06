2c01179f569243c7ef5fb7a4ad9dffcd50ba0c37
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-11-06 21:59:42 +0700

    Fetch campain data with open status for edit only

--- b2d-venture/app/dashboard/business/[id]/edit-raise-campaign/page.tsx
+++ b2d-venture/app/dashboard/business/[id]/edit-raise-campaign/page.tsx
@@ -2,11 +2,11 @@ import DragAndDrop from "@/components/shared/BusinessDashboard/DragAndDrop";
 import FileContainer from "@/components/shared/BusinessDashboard/FileContainer";
 import DataRoom from "@/models/DataRoom";
 import Business from "@/models/Business";
-import connect from "@/lib/connectDB"
+import connect from "@/lib/connectDB";
 import { EditRaiseCampaignForm } from "@/components/shared/BusinessDashboard/EditRaiseCampaign";
 
 export default async function Page({ params }) {
-  const {id} = params;
+  const { id } = params;
   const campaignUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/fetchingData/RaiseCampaign/businessId/${id}`;
   try {
     const response = await fetch(campaignUrl);
@@ -14,11 +14,16 @@ export default async function Page({ params }) {
       throw new Error("Failed to fetch raise campaign data");
     }
     const data = await response.json();
-    const campaignData = data[0];
 
-    return(
-        <EditRaiseCampaignForm params={campaignData._id} data={campaignData}/>
-      );
+    const openCampaignData = data.find(campaign => campaign.status === "open");
+
+    if (!openCampaignData) {
+      return <p>No open raise campaign found for this business.</p>;
+    }
+
+    return (
+      <EditRaiseCampaignForm params={openCampaignData._id} data={openCampaignData} />
+    );
   } catch (error) {
     console.error("Error fetching campaign data:", error);
     return <p>Failed to load campaign data.</p>;
