50278654b720fdd613f7e1139780234252ab86e0
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2024-11-12 19:48:14 +0700

    Limit access to dashboard

--- b2d-venture/middleware.ts
+++ b2d-venture/middleware.ts
@@ -1,45 +1,38 @@
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-import User from '@/models/user';
-import Business from '@/models/Business';
 
 export async function middleware(req) {
-  // console.log(req)
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  // console.log('Token:', token);
-  // token gives
-  // Token: {
-  //   email: 'mark@gmail.com',
-  //   role: 'business',
-  //   iat: 1729653881,
-  //   exp: 1732245881,
-  //   jti: 'f4cd23d8-1575-4272-a201-26a40f934870'
-  // }
-
   const { pathname } = req.nextUrl;
 
+  // Redirect to login if the user is not authenticated
   if (!token) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
-  //  if (pathname.startsWith('/dashboard/admin')) {
-  //    if (token.role !== 'admin') {
-  //      return NextResponse.redirect(new URL('/403', req.url));
-  //    }
-  //  }
+  // Admins can access any route
+  if (token.role === 'admin') {
+    return NextResponse.next();
+  }
 
+  // Business dashboard access control
+  if (pathname.startsWith('/dashboard/business')) {
+    const pathParts = pathname.split('/');
+    const businessIdFromPath = pathParts[3]; // Extract business ID from the path
 
-  // if (pathname.startsWith('/dashboard/business')) {
-  //   const pathParts = pathname.split('/');
-  //   const businessId = pathParts[3];
-  //   if (token.role !== 'business') {
-  //     return NextResponse.redirect(new URL('/403', req.url));
-  //   }
-  // }
+    // Check if the user is a business user and if they are accessing their own dashboard
+    if (token.role !== 'business' || token.businessId !== businessIdFromPath) {
+      return NextResponse.redirect(new URL('/403', req.url));
+    }
+  }
 
-
+  // Investor dashboard access control
   if (pathname.startsWith('/dashboard/investor')) {
-    if (token.role !== 'investor') {
+    const pathParts = pathname.split('/');
+    const investorIdFromPath = pathParts[3]; // Extract investor ID from the path
+
+    // Check if the user is an investor and if they are accessing their own dashboard
+    if (token.role !== 'investor' || token.investorId !== investorIdFromPath) {
       return NextResponse.redirect(new URL('/403', req.url));
     }
   }
